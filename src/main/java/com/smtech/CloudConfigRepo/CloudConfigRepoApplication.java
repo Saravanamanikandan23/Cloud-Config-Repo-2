191f887c38af6e417e67bdae5c8052ad61ea9868
Saravanamanikandan23 <ssmanikandan200@gmail.com>
2024-05-16 14:30:30 +0530

    Code Alignment and Name Tag Added

--- src/main/java/com/smtech/CloudConfigRepo/CloudConfigRepoApplication.java
+++ src/main/java/com/smtech/CloudConfigRepo/CloudConfigRepoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudConfigRepoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudConfigRepoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigRepoApplication.class, args);
+    }
 
 }
